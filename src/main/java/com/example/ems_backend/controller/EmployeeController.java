68614c88e6ca2e262820bbba4a821334a9ab4376
arasu20 <imarasu20@gmail.com>
2025-01-02 19:42:36 +0530

    CrossOrigin added

--- src/main/java/com/example/ems_backend/controller/EmployeeController.java
+++ src/main/java/com/example/ems_backend/controller/EmployeeController.java
@@ -16,14 +16,13 @@ import lombok.AllArgsConstructor;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 
-
-
-
+@CrossOrigin("*")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/employees")
